f71328d484d176808f3324eddfe7a88b2f412f78
Ahmed Badr <ahmed.k.badr.97@gmail.com>
2023-01-09 16:34:56 +0200

    do bulk preprocessing for raw  data and save the preprocessed data
    create nn-sales model dataloader

---
+++ src/data_loader.py
@@ -0,0 +1,27 @@
+import torch
+from torch.utils.data.dataset import Dataset
+import pandas as pd
+
+
+class NNSalesDataset(Dataset):
+    def __init__(self, sales_df: pd.DataFrame):
+        sales_data = sales_df
+        # drop Store id column
+        sales_data = sales_data.drop('Store', axis=1)
+        self.size = len(sales_data)
+
+        # get numpy target column
+        self.sales_data_target = sales_data['Sales'].to_numpy()
+
+        # get numpy input columns
+        sales_data.drop("Sales", axis=1, inplace=True)
+
+        self.no_cols = len(sales_data.columns)
+        self.sales_data_input = sales_data.to_numpy()
+
+    def __getitem__(self, idx):
+        return torch.tensor(self.sales_data_input[idx], dtype=torch.float), torch.tensor(self.sales_data_target[idx],
+                                                                                         dtype=torch.float)
+
+    def __len__(self):
+        return self.size

17449b12422600cfaca666d81010a6d77056c294
Ahmed Badr <ahmed.k.badr.97@gmail.com>
2023-01-14 13:22:29 +0200

    fix dataloader sequence generator

--- src/data_loader.py
+++ src/data_loader.py
@@ -40,13 +40,14 @@ class LSTMSalesDataset(IterableDataset):
     lstm_sales_cols = {col_name: idx for idx, col_name in enumerate(lstm_sales_cols)}
     lstm_store_cols = {col_name: idx for idx, col_name in enumerate(lstm_store_cols)}
 
-    def __init__(self, store_sales, store_data, seq_length=30):
+    def __init__(self, store_sales, store_data, seq_length=30, future_out=30):
         """
-
+        future
         :param merged_data_df:
         :param lstm_sales_cols: col name to index dictionary for lstm sales data
         :param lstm_store_cols: col name to index dictionary for store data for nn of the lstm
         """
+        self.future_out = future_out
         self.stores_sales = []
 
         self.seq_length = seq_length
@@ -57,7 +58,16 @@ class LSTMSalesDataset(IterableDataset):
 
         self.no_lstm_cols = len(LSTMSalesDataset.lstm_sales_cols)
         self.no_store_data_cols = len(LSTMSalesDataset.lstm_store_cols)
-        self.size = len(store_sales)-seq_length
+
+        self.size = self.get_size()
+
+        # sequences
+
+        # the last valid window is the window-size + 1 + the len of future output
+        # ex size=8 and seq_len=2 , future output=3 last valid idx for window is 3
+        # 0 1 2 3 4 5 6 7
+        # _ _ _ _ _ _ _ _
+        #       |_| |___|
 
     def __iter__(self):
         random.shuffle(self.stores_indices)
@@ -66,20 +76,19 @@ class LSTMSalesDataset(IterableDataset):
             store_data = self.stores_data_dict[store_idx]['data']
             store_sales = self.stores_data_dict[store_idx]['sales']
 
-            no_sequences = len(store_sales) - self.seq_length
-
-            in_idx, out_idx = 0, self.seq_length
+            # out starts from seq_len +1 but we are zero based so seq_len +1 -1 = seq_len
+            out_idx = self.seq_length
+            no_sequences = (len(store_sales) - self.seq_length - self.future_out + 1)
             for i in range(no_sequences):
+                # from window_idx to seq_len (slicing end is exclusive)
                 lstm_in = torch.tensor(store_sales[i:i + self.seq_length], dtype=torch.float)
                 nn_in = torch.tensor(store_data.flatten(), dtype=torch.float)
 
-                in_idx += self.seq_length
-
                 # get the sales column only
-                out = store_sales[out_idx:out_idx + self.seq_length, self.lstm_sales_cols['Sales']]
+                out = store_sales[out_idx:out_idx + self.future_out, self.lstm_sales_cols['Sales']]
                 # out = np.expand_dims(out, axis=0).reshape((self.seq_length, 1))
                 out = torch.tensor(out, dtype=torch.float)
-                out_idx += self.seq_length
+                out_idx += 1
 
                 yield lstm_in, nn_in, out
 
@@ -108,3 +117,9 @@ class LSTMSalesDataset(IterableDataset):
 
     def __len__(self):
         return self.size
+
+    def get_size(self):
+        sequences_sum = 0
+        for store_data in self.stores_data_dict.values():
+            sequences_sum += (len(store_data['sales'])-self.future_out-self.seq_length +1)
+        return sequences_sum
